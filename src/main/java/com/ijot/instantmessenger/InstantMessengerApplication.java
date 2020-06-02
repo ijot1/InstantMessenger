84fe6b1a1b0c2bd636416cee15bc48a9cb45b8da
ijot1 <ireneuszjaneczek128@gmail.com>
2020-06-03 00:08:46 +0200

    Initial commit

---
+++ src/main/java/com/ijot/instantmessenger/InstantMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.ijot.instantmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstantMessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ijot/instantmessenger/InstantMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijot.instantmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
